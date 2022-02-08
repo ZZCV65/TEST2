7d8aa21ea9e507bb6679f1fd3b0264e82deeb5b2
ZZCV65 <99249959+ZZCV65@users.noreply.github.com>
2022-02-08 19:06:52 +0700

    Update DW HUB.lua

--- DW HUB.lua
+++ DW HUB.lua
@@ -5,12 +5,6 @@ local Window = Library.CreateLib("DW HUB", "Midnight")
 
 local Tab = Window:NewTab("Main Menu")
 local Section = Tab:NewSection("Teleport")
-Section:NewButton("Spawn", "ButtonInfo", function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0.123497024, 3.99802542, 0.344314158, 0.999047995, 3.46464226e-08, -0.0436239466, -3.89375288e-08, 1, -9.75161427e-08, 0.0436239466, 9.91219196e-08, 0.999047995)
-end)
-Section:NewButton("Green", "ButtonInfo", function()
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(78.9791794, 3.98771453, -57.2457352, -0.608757913, 1.63033036e-08, 0.793356001, -7.25029237e-09, 1, -2.61130904e-08, -0.793356001, -2.16486136e-08, -0.608757913)
-end)
 
 
 
